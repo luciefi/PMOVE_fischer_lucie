6b7abf62a127f50a190152f78ca8f3797f51c425
Lucie <lucie.fischer@outlook.com>
2021-12-11 22:39:54 +0100

    added 30 minutes free parking

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -12,7 +12,7 @@ public class FareCalculatorService {
 
         float durationInMilliseconds = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
         float durationInHour = durationInMilliseconds / (1000 * 3600);
-
+        
         if(durationInHour < .5){
             ticket.setPrice(0.0);
             return;
